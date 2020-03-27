df0053055dec667779aac158e13ac80ad68c5f1c
Gerasimov Ruslan <gerassimov.ruslan@gmail.coom>
2020-03-28 00:26:31 +0300

    express routing changed

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const express = require('express');
 const app = express();
 const { checkRepository }  = require('./backend/api/builds');
 
-const staticRoute = require('./config/routes/static/index');
 const apiSettingsRoute = require('./config/routes/api/settings');
 const apiBuildsRoute = require('./config/routes/api/builds');
 const testRoute = require('./config/routes/test-api');
 
-app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.use('/', staticRoute);
+app.use('/static', express.static('public'));
 app.use('/api', apiSettingsRoute, apiBuildsRoute);
 app.use('/test-api', testRoute);
 
-app.listen(3000, function () {
-    console.log('App listening on port 3000!');
+app.listen(process.env.SERVER_PORT, function () {
+    console.log(`App listening on port ${process.env.SERVER_PORT}!`);
 });
 
 checkRepository();
