b5ec65709a44b2b94f5dc4c067a2b68e5e771591
Gerasimov Ruslan <gerassimov.ruslan@gmail.coom>
2020-04-04 21:01:53 +0300

    added react-to-ansi

--- client/src/components/Log/Log.js
+++ client/src/components/Log/Log.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {cn} from "@bem-react/classname";
+import Ansi from 'ansi-to-react';
+
 
 import "./Log.css";
 
 export default (props) => {
     return (
-        <div className={cn('Log')()}>
-            {props.children}
+        <div  className={cn('Log')()} >
+            <Ansi>
+                {props.children}
+            </Ansi>
         </div>
     )
 }
